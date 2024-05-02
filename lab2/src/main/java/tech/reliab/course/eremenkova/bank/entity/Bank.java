25332e96a97fd4f4670bff54c5c6afc33bc74a23
MisterShady <shadeytepta@gmail.com>
2024-05-02 10:48:51 +0300

    Пересобрана лаб 2

---
+++ lab2/src/main/java/tech/reliab/course/eremenkova/bank/entity/Bank.java
@@ -0,0 +1,97 @@
+package tech.reliab.course.eremenkova.bank.entity;
+
+import java.util.List;
+
+public class Bank {
+    private long id;
+    private String name;
+    private List<BankOffice> bankOfficeList;
+    private List<BankAtm> bankAtmList;
+    private List<Employee> employeeList;
+    private List<User> userList;
+    private int bankRating;
+    private double totalMoneyInBank;
+    private double interestRate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<BankOffice> getBankOfficeList() {
+        return bankOfficeList;
+    }
+
+    public void setBankOfficeList(List<BankOffice> bankOfficeList) {
+        this.bankOfficeList = bankOfficeList;
+    }
+
+    public List<BankAtm> getBankAtmList() {
+        return bankAtmList;
+    }
+
+    public void setBankAtmList(List<BankAtm> bankAtmList) {
+        this.bankAtmList = bankAtmList;
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    public int getBankRating() {
+        return bankRating;
+    }
+
+    public void setBankRating(int bankRating) {
+        this.bankRating = bankRating;
+    }
+
+    public double getTotalMoneyInBank() {
+        return totalMoneyInBank;
+    }
+
+    public void setTotalMoneyInBank(double totalMoneyInBank) {
+        this.totalMoneyInBank = totalMoneyInBank;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "\nid=" + id +
+                ", \nname='" + name + '\'' +
+                ", \nbankRating=" + bankRating +
+                ", \ntotalMoneyInBank=" + totalMoneyInBank +
+                ", \ninterestRate=" + interestRate;
+    }
+}
---
+++ lab2/src/main/java/tech/reliab/course/eremenkova/bank/entity/PaymentAccount.java
@@ -0,0 +1,49 @@
+package tech.reliab.course.eremenkova.bank.entity;
+
+public class PaymentAccount {
+    private long id;
+    private User user;
+    private String bankName;
+    private double balance;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "\nid=" + id +
+                ", \nuserId=" + user.getId() +
+                ", \nbankName='" + bankName + '\'' +
+                ", \nbalance=" + balance;
+    }
+}
